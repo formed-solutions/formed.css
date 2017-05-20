4f8ad2db946426ce6835da74d5b181620d4fba12
Chris DeFelice <christopherdefelice@wi-design.com>
2017-05-20 12:56:52 -0400

    feat(components): menu
    Create component that represents a list of options a user can initiate.  Deprecate version of Menu.

--- src/formed.js
+++ src/formed.js
@@ -1,11 +1,13 @@
 import checkbox from './components/checkbox';
 import editor from './components/editor';
 import layout from './components/layout';
+import menu from './components/menu';
 import radio from './components/radio';
 
 export default {
   checkbox,
   editor,
   layout,
+  menu,
   radio
 };
